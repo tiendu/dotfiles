da5fbbdee905dabcc846d6c1d214f0eaa5dad0a3
Tien Du <52783290+tiendu@users.noreply.github.com>
2024-11-28 16:24:09 +0700

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,47 +3,55 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- Appearance settings
-config.color_scheme = 'AdventureTime'       -- Set color scheme
-config.font = wezterm.font_with_fallback {  -- Set font with fallback options
-  'JetBrains Mono',                         -- Primary font
-  'Fira Code',                              -- Fallback font
-  'Noto Color Emoji',                       -- Emoji support
+config.color_scheme = 'Dracula'
+config.font = wezterm.font_with_fallback {
+  weight = 'Bold'
 }
-config.font_size = 16.0                     -- Font size
-config.window_background_opacity = 0.8      -- Background transparency
+config.font_size = 16.0
+config.window_background_opacity = 0.8
 
 -- Tab bar customization
-config.hide_tab_bar_if_only_one_tab = true  -- Hide tab bar when only one tab
-config.use_fancy_tab_bar = false            -- Disable fancy tab bar for minimalistic look
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = true
+config.tab_bar_style = {
+  font_size = 19.0,
+}
+config.colors = {
+  tab_bar = {
+    background = '#282828',
+    active_tab = { bg_color = '#458588', fg_color = '#ebdbb2' },
+    inactive_tab = { bg_color = '#3c3836', fg_color = '#a89984' },
+  },
+}
 
 -- Cursor and Scrollback settings
-config.cursor_blink_rate = 500              -- Set cursor blink rate (in ms)
-config.scrollback_lines = 10000             -- Set scrollback buffer size
+config.default_cursor_style = 'SteadyBlock'
+config.scrollback_lines = 10000
 
 -- Keybindings
 config.keys = {
-  { key = 't', mods = 'CTRL|SHIFT', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },  -- New tab
-  { key = 'w', mods = 'CTRL|SHIFT', action = wezterm.action.CloseCurrentTab { confirm = true } },  -- Close tab
-  { key = '"', mods = 'CTRL|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } }, -- Split pane vertically
-  { key = '%', mods = 'CTRL|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } }, -- Split pane horizontally
-  { key = 'h', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Left' },  -- Move to left pane
-  { key = 'l', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Right' }, -- Move to right pane
-  { key = 'j', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Down' },  -- Move to lower pane
-  { key = 'k', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Up' },    -- Move to upper pane
+  { key = 't', mods = 'CTRL|SHIFT', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
+  { key = 'w', mods = 'CTRL|SHIFT', action = wezterm.action.CloseCurrentTab { confirm = true } },
+  { key = '"', mods = 'CTRL|SHIFT', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+  { key = '%', mods = 'CTRL|SHIFT', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+  { key = 'h', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Left' },
+  { key = 'l', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Right' },
+  { key = 'j', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Down' },
+  { key = 'k', mods = 'CTRL|SHIFT', action = wezterm.action.ActivatePaneDirection 'Up' },
 }
 
 -- General behavior settings
-config.check_for_updates = false            -- Disable automatic updates
-config.audible_bell = 'Disabled'            -- Disable audible bell
-config.enable_wayland = false               -- Disable Wayland support for better compatibility
+config.check_for_updates = false
+config.audible_bell = 'Disabled'
+config.enable_wayland = false
 
 -- Padding and window settings
-config.window_padding = {                   -- Set padding around the terminal
-  left = 5,
-  right = 5,
-  top = 5,
-  bottom = 5,
+config.window_padding = {
+  left = 10,
+  right = 10,
+  top = 10,
+  bottom = 10,
 }
-config.window_decorations = "RESIZE"        -- Remove title bar and only allow resizing
+config.window_decorations = "RESIZE"
 
 return config
