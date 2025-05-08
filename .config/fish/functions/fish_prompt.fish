1ea45d988b5e5ba72d96f2853305e0732e7a344c
𝚝𝚒𝚎𝚗𝚍𝚞 <52783290+tiendu@users.noreply.github.com>
2025-05-08 16:26:51 +0700

    Update fish_prompt.fish

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -2,16 +2,33 @@ function fish_prompt
     set -l last_pipestatus $pipestatus
     set -lx __fish_last_status $status
     set -l normal (set_color normal)
+    set -l green (set_color green)
     set -q fish_color_status
     or set -g fish_color_status red
 
     set -l color_cwd $fish_color_cwd
-    set -l suffix '›'
+
+    # Default suffix and color (non-root, success)
+    set -l suffix '❯'
+    set -l suffix_color (set_color green)
+
+    # Root user?
     if functions -q fish_is_root_user; and fish_is_root_user
         if set -q fish_color_cwd_root
             set color_cwd $fish_color_cwd_root
         end
-        set suffix '»'
+        set suffix '➤'
+        if test $__fish_last_status -eq 0
+            set suffix_color (set_color --bold yellow)
+        else
+            set suffix_color (set_color --bold red)
+        end
+    else
+        # Normal user
+        if test $__fish_last_status -ne 0
+            set suffix_color (set_color red)
+            set suffix '⚠'
+        end
     end
 
     set -l bold_flag --bold
@@ -27,6 +44,6 @@ function fish_prompt
     # Line 1: user@host full-path vcs dir-info status
     echo -n -s (prompt_login) ' ' (set_color $color_cwd) (pwd) $normal (fish_vcs_prompt) ' ' (get_dir_info) ' ' $prompt_status
     echo ""
-    echo -n -s $suffix " "
+    echo -n -s $suffix_color$suffix$normal " "
 end
 
