3cd2134055dc61da93761f4d94c2f13711cdda29
tiendu <tiendu107@gmail.com>
2025-07-15 21:38:43 +0700

    fix: better color scheme

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,13 +3,13 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 -- Appearance settings
-config.color_scheme = 'OneHalfDark'  -- or 'Gruvbox Dark', 'Dracula', 'Builtin Solarized Dark'
+config.color_scheme = 'Homebrew'
 config.font = wezterm.font_with_fallback {
   weight = 'Bold'
 }
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }  -- Turn off ligatures
 config.font_size = 16.0
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.75
 
 -- Tab bar customization
 config.hide_tab_bar_if_only_one_tab = true
